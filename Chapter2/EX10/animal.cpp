04a941480e793f1cfcb98165e3bf29dccaa21a6b
ZhiHua Lai <rush495075830@live.com>
2018-07-04 21:50:29 +0800

    02

--- Chapter2/EX10/animal.cpp
+++ Chapter2/EX10/animal.cpp
@@ -1,5 +1,7 @@
+//编译器版本更新修改：项目》属性>C/C++>代码生成>启用函数级连接>是 (/Gy)
+#include <iostream>		//#include <iostream.h>应该修改为#include <iostream>
 #include "animal.h"
-#include <iostream.h>
+using namespace std;		//不加此名会报C2065错误
 animal::animal()
 {
 }
@@ -18,5 +20,28 @@ void animal::sleep()
 
 void animal::breathe()
 {
-	cout<<"animal breathe"<<endl;
+	cout << "animal breathe" << endl;
 }
+//原程序
+//#include "animal.h"
+//#include <iostream.h>
+//animal::animal()
+//{
+//}
+//
+//animal::~animal()
+//{
+//}
+//
+//void animal::eat()
+//{
+//}
+//
+//void animal::sleep()
+//{
+//}
+//
+//void animal::breathe()
+//{
+//	cout<<"animal breathe"<<endl;
+//}
--- Chapter2/EX10/fish.cpp
+++ Chapter2/EX10/fish.cpp
@@ -1,6 +1,15 @@
+//编译器版本更新修改：项目》属性>C/C++>代码生成>启用函数级连接>是 (/Gy)
+#include <iostream>		//#include <iostream.h>应该修改为#include <iostream>
 #include "fish.h"
-#include <iostream.h>
+using namespace std;		//不加此名会报C2065错误
 void fish::breathe()
 {
-	cout<<"fish bubble"<<endl;
+	cout << "fish bubble" << endl;
 }
+//原程序
+//#include "fish.h"
+//#include <iostream.h>
+//void fish::breathe()
+//{
+//	cout<<"fish bubble"<<endl;
+//}
